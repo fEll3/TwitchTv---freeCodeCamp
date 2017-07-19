5accc6746167aa99262aacaee715a284bc970047
fEll3 <MitraWebTechnlogy@gmail.com>
2017-07-19 17:48:45 +0100

    adding JS All and Online users without styling

--- js/js.js
+++ js/js.js
@@ -1,3 +1,41 @@
-var path = document.querySelector(".path");
-var length = path.getTotalLength();
-console.log(length);
+$(function(){
+
+  var $all = $(".all");
+  var $online = $(".online");
+  var $offline = $(".offline");
+  var $list = $(".list");
+  var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var urlStreams =  'https://wind-bow.gomix.me/twitch-api/streams/';
+  var urlChannel = "https://wind-bow.gomix.me/twitch-api/channels/";
+
+  function userData(xxx){
+    $list.html("");
+    for (var i = 0; i < xxx.length; i++) {
+      $.getJSON( urlChannel + xxx[i] + "?callback=?", function(user){
+        var usersData = [user];
+        for (var i = 0; i < usersData.length; i++) {
+          $list.append("<ul><li>" + "<a href=\"" + usersData[i].url
+          + "\" target='_blank'><img src=\"" + usersData[i].logo + "\"></a>"
+          +"<h3>" + usersData[i].name + "</h3><p>" + usersData[i].status + "</p></li></ul>");
+        }
+      });
+    }
+  };
+
+  $all.click(function(){
+    userData(userNames);
+  });
+
+  $online.click(function(){
+    for (var i = 0; i < userNames.length; i++) {
+      $.getJSON( urlStreams + userNames[i] + "?callback=?", function(data){
+        if(data.stream != null){
+          var onlineArray = [];
+          onlineArray.push(data.stream.channel.name);
+        }
+        userData(onlineArray);
+      });
+    }
+  });
+
+});
