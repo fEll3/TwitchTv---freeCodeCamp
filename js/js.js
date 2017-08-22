f98fdb45d1c8e8ae9b6608dd69e0dee88abb4905
fEll3 <MitraWebTechnlogy@gmail.com>
2017-08-22 15:33:12 +0100

    stage2

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ $(function(){
   var $online = $(".online");
   var $offline = $(".offline");
   var $list = $(".list");
-  var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
   var urlStreams =  'https://wind-bow.gomix.me/twitch-api/streams/';
   var urlChannel = "https://wind-bow.gomix.me/twitch-api/channels/";
 
@@ -14,7 +14,7 @@ $(function(){
       $.getJSON( urlChannel + xxx[i] + "?callback=?", function(user){
         var usersData = [user];
         for (var i = 0; i < usersData.length; i++) {
-          $list.append("<ul><li>" + "<a href=\"" + usersData[i].url
+          $list.append("<ul><li class='scrolls'>" + "<a href=\"" + usersData[i].url
           + "\" target='_blank'><img src=\"" + usersData[i].logo + "\"></a>"
           +"<h3>" + usersData[i].name + "</h3><p>" + usersData[i].status + "</p></li></ul>");
         }
@@ -32,9 +32,34 @@ $(function(){
         if(data.stream != null){
           var onlineArray = [];
           onlineArray.push(data.stream.channel.name);
+          if(i < userNames.length);
+          userData(onlineArray);
         }
-        userData(onlineArray);
       });
+    }
+  });
+
+  $offline.click(function(){
+    for (let i = 0; i < userNames.length; i++) {
+      $.ajax({
+        url: urlStreams + userNames[i] + "?callback=?",
+        dataType: "json",
+        type: "GET",
+        success: function(x){
+          if(x.stream === null){
+            $.ajax({
+              url: urlChannel + userNames[i] + "?callback=?",
+              dataType: "json",
+              type: "GET",
+              success: function(y){
+                var offlineArray = [];
+                offlineArray.push(y.name);
+                userData(offlineArray);
+              }
+            })
+          }
+        }
+      })
     }
   });
 
