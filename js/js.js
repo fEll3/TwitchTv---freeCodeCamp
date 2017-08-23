1ba8f2d2c4d80a5834039f44b81c1ef6b586354f
fEll3 <MitraWebTechnlogy@gmail.com>
2017-08-23 19:04:03 +0100

    Diffrent aproach using function to toogle between API

--- js/js.js
+++ js/js.js
@@ -1,66 +1,72 @@
 $(function(){
 
-  var $all = $(".all");
-  var $online = $(".online");
-  var $offline = $(".offline");
+  // Variabels
+  var $buttons = $(".button");
+  var $buttonsContainer = $(".item");
   var $list = $(".list");
-  var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   var urlStreams =  'https://wind-bow.gomix.me/twitch-api/streams/';
   var urlChannel = "https://wind-bow.gomix.me/twitch-api/channels/";
 
-  function userData(xxx){
-    $list.html("");
-    for (var i = 0; i < xxx.length; i++) {
-      $.getJSON( urlChannel + xxx[i] + "?callback=?", function(user){
-        var usersData = [user];
-        for (var i = 0; i < usersData.length; i++) {
-          $list.append("<ul><li class='scrolls'>" + "<a href=\"" + usersData[i].url
-          + "\" target='_blank'><img src=\"" + usersData[i].logo + "\"></a>"
-          +"<h3>" + usersData[i].name + "</h3><p>" + usersData[i].status + "</p></li></ul>");
-        }
-      });
-    }
-  };
-
-  $all.click(function(){
-    userData(userNames);
-  });
-
-  $online.click(function(){
-    for (var i = 0; i < userNames.length; i++) {
-      $.getJSON( urlStreams + userNames[i] + "?callback=?", function(data){
-        if(data.stream != null){
-          var onlineArray = [];
-          onlineArray.push(data.stream.channel.name);
-          if(i < userNames.length);
-          userData(onlineArray);
-        }
-      });
-    }
-  });
-
-  $offline.click(function(){
+  // API request using JSON for Twitch Tv
+  (function (){
     for (let i = 0; i < userNames.length; i++) {
-      $.ajax({
-        url: urlStreams + userNames[i] + "?callback=?",
-        dataType: "json",
-        type: "GET",
-        success: function(x){
-          if(x.stream === null){
-            $.ajax({
-              url: urlChannel + userNames[i] + "?callback=?",
-              dataType: "json",
-              type: "GET",
-              success: function(y){
-                var offlineArray = [];
-                offlineArray.push(y.name);
-                userData(offlineArray);
-              }
-            })
-          }
+      $.getJSON(urlStreams + userNames[i] + "?callback=?", function(data){
+        console.log(data);
+        // Offlline
+        if(data.stream === null){
+          $.getJSON(urlChannel + userNames[i] + "?callback=?", function(data2){
+            console.log(data2);
+            if(data2.error){
+              $list.append('<div class="dataList"><Error</div>');
+            } else {
+              $list.append('<div class="dataList dataOffline"><div class="block"><a href="' +
+              data2.url + '"target="_blank"><img src="' +
+              data2.logo + '"></a></div><div class="block align"><h3 class="name">' + data2.name +
+              '</h3><p class="dataStatus">' + data2.status +
+              '</p></div><div class="block align"><p class="status">Offline</p></div></div>');
+            }
+          })
+        // Online
+        } else {
+          $list.append('<div class="dataList dataOnline"><div class="block"><a href="' +
+          data.stream.channel.url + '"target="_blank"><img src="' +
+          data.stream.channel.logo + '"></a></div><div class="block align"><h3 class="name">' +
+          data.stream.channel.name + ' ' + data.stream.game +
+          '</h3><p class="dataStatus">' + data.stream.channel.status +
+          '</p></div><div class="block align"><p class="status">Online</p></div></div>');
         }
-      })
+      });
     }
-  });
+  }());
+
+  // Function to toogle between API result by adding and removing class Active
+  (function toogle(){
+    $buttons.on('click', function(){
+      var $results = $(".list .dataList");
+      $buttonsContainer.removeClass("active");
+      if($(this).hasClass("online")){
+        $(this).parent().addClass("active");
+        $results.hide();
+        $results.each(function(){
+          if($(this).hasClass("dataOnline")){
+            $(this).show();
+          }
+        });
+      } else if($(this).hasClass("offline")){
+        $(this).parent().addClass("active");
+        $results.hide();
+        $results.each(function(){
+          if($(this).hasClass("dataOffline")){
+            $(this).show();
+          }
+        });
+      } else if($(this).hasClass("all")){
+        $(this).parent().addClass("active");
+        $results.hide();
+        $results.show();
+      }
+    });
+  }());
 
 });
